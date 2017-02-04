2d88ef6a979b5769080301475e6d715d3bb0deb1
Helge Eichhorn <git@helgeeichhorn.de>
2017-02-04 14:32:03 +0100

    Implement libration point coordinate frames.

--- src/libration.f90
+++ src/libration.f90
@@ -1,14 +1,17 @@
 module libration
 
+use bodies, only: body
+use ephemerides, only: ephem, getstate
+use epochs, only: epoch
 use exceptions
-use math, only: findroot
+use math, only: findroot, cross, norm
 use types, only: dp
 
 implicit none
 
 private
 
-public :: librationdist
+public :: librationdist, gcrftolib, libtogcrf
 
 contains
 
@@ -45,16 +48,17 @@ function l3dist(x, rpar, ipar) result(res)
     res = x**3._dp - (1._dp - mu) * (1._dp + x)**2._dp / (1._dp + 2._dp * mu + x * (2._dp + mu + x))
 end function l3dist
 
-function librationdist(mu_prime, mu_sec, point) result(res)
+function librationdist(mu_prime, mu_sec, point, err) result(res)
     real(dp), intent(in) :: mu_prime
     real(dp), intent(in) :: mu_sec
     character(len=*), intent(in) :: point
+    type(exception), intent(inout), optional :: err
     real(dp) :: res
 
     real(dp), dimension(1) :: rpar
     real(dp) :: mu_lib
     real(dp) :: initial
-    type(exception) :: err
+    type(exception) :: err_
 
     res = 0._dp
     mu_lib = 1._dp / (1._dp + mu_prime / mu_sec)
@@ -71,9 +75,125 @@ function librationdist(mu_prime, mu_sec, point) result(res)
         initial = 1._dp - (7._dp * mu_lib / 12._dp)
         res = findroot(l3dist, initial, rpar=rpar)
     case default
-        err = error("Libration point name must be 'L1', 'L2', or 'L3'.", "librationdist", __FILE__, __LINE__)
-        call raise(err)
+        err_ = error("Libration point name must be 'L1', 'L2', or 'L3'.", "librationdist", __FILE__, __LINE__)
+        if (present(err)) then
+            err = err_
+        else
+            call raise(err_)
+        end if
     end select
 end function librationdist
+
+subroutine rotationparams(rv, m, rm, omega)
+    real(dp), dimension(:), intent(in) :: rv
+    real(dp), dimension(:,:), intent(out) :: m
+    real(dp), intent(out) :: rm
+    real(dp), intent(out) :: omega
+
+    real(dp), dimension(3) :: omegav
+    real(dp) :: om
+
+    m = 0._dp
+    rm = norm(rv(1:3))
+    m(:,1) = rv(1:3) / rm
+    omegav = cross(rv(1:3), rv(4:6))
+    om = norm(omegav)
+    m(:,3) = omegav / om
+    m(:,2) = cross(m(:,3), m(:,1))
+    omega = om / rm**2
+end subroutine rotationparams
+
+function gcrftolib(rv, ep, primary, secondary, point, err) result(lib)
+    real(dp), dimension(:), intent(in) :: rv
+    type(epoch), intent(in) :: ep
+    type(body), intent(in) :: primary
+    type(body), intent(in) :: secondary
+    character(len=*), intent(in) :: point
+    type(exception), intent(inout), optional :: err
+    real(dp), dimension(6) :: lib
+
+    type(exception) :: err_
+    real(dp) :: gam
+    real(dp), dimension(6) :: sec
+    real(dp), dimension(3,3) :: m
+    real(dp) :: rm
+    real(dp) :: omega
+
+    gam = librationdist(primary%mu, secondary%mu, point, err=err_)
+    if (iserror(err_)) then
+        call catch(err_, "gcrftolib", __FILE__, __LINE__)
+        if (present(err)) then
+            err = err_
+            return
+        else
+            call raise(err_)
+        end if
+    end if
+    sec = ephem%state(ep, secondary%id, primary%id/100, err_)
+    if (iserror(err_)) then
+        call catch(err_, "gcrftolib", __FILE__, __LINE__)
+        if (present(err)) then
+            err = err_
+            return
+        else
+            call raise(err_)
+        end if
+    end if
+    lib = rv - sec * (1 + gam)
+
+    call rotationparams(sec, m, rm, omega)
+    lib(1:3) = matmul(transpose(m), lib(1:3))
+    lib(4:6) = matmul(transpose(m), lib(4:6)) - [-omega * lib(2), omega * lib(1), 0._dp]
+    lib(1:3) = lib(1:3) / rm
+    lib(4:6) = lib(4:6) / (rm * omega)
+end function gcrftolib
+
+function libtogcrf(lib, ep, primary, secondary, point, err) result(rv)
+    real(dp), dimension(:), intent(in) :: lib
+    type(epoch), intent(in) :: ep
+    type(body), intent(in) :: primary
+    type(body), intent(in) :: secondary
+    character(len=*), intent(in) :: point
+    type(exception), intent(inout), optional :: err
+    real(dp), dimension(6) :: res
+
+    type(exception) :: err_
+    real(dp) :: gam
+    real(dp), dimension(6) :: sec
+    real(dp), dimension(6) :: rv
+    real(dp), dimension(3,3) :: m
+    real(dp) :: rm
+    real(dp) :: omega
+
+    gam = librationdist(primary%mu, secondary%mu, point, err=err_)
+    if (iserror(err_)) then
+        call catch(err_, "libtogcrf", __FILE__, __LINE__)
+        if (present(err)) then
+            err = err_
+            return
+        else
+            call raise(err_)
+        end if
+    end if
+    sec = ephem%state(ep, secondary%id, primary%id/100, err_)
+    if (iserror(err_)) then
+        call catch(err_, "libtogcrf", __FILE__, __LINE__)
+        if (present(err)) then
+            err = err_
+            return
+        else
+            call raise(err_)
+        end if
+    end if
+
+    call rotationparams(sec, m, rm, omega)
+    rv = lib
+    rv(4:6) = rv(4:6) + [-lib(2), lib(1), 0._dp]
+    rv(1:3) = matmul(m, rv(1:3))
+    rv(4:6) = matmul(m, rv(4:6))
+    rv(1:3) = rv(1:3) * rm
+    rv(4:6) = rv(4:6) * omega * rm
+    rv = rv + sec * (1 + gam)
+end function libtogcrf
 
 end module libration
--- test/test_libration.f90
+++ test/test_libration.f90
@@ -2,6 +2,8 @@ program testlibration
 
 use assertions
 use constants
+use epochs, only: epoch
+use ephemerides, only: init_ephemeris
 use libration
 use types, only: dp
 
@@ -13,17 +15,33 @@ real(dp) :: l3ref
 real(dp) :: l1dist
 real(dp) :: l2dist
 real(dp) :: l3dist
+real(dp), dimension(6) :: ref
+real(dp), dimension(6) :: rv
+real(dp), dimension(6) :: lib
+real(dp), dimension(6) :: eci
+type(epoch) :: ep
 
 call init_constants
+call init_ephemeris
 
 l1ref = 0.15093428265127021_dp
 l1dist = librationdist(earth%mu, moon%mu, "L1")
-! call assert_almost_equal(l1ref, l1dist, __LINE__)
+call assert_almost_equal(l1ref, l1dist, __LINE__)
 l2ref = 0.16783274367911588_dp
 l2dist = librationdist(earth%mu, moon%mu, "L2")
 call assert_almost_equal(l2ref, l2dist, __LINE__)
 l3ref = 0.99291206108848129_dp
 l3dist = librationdist(earth%mu, moon%mu, "L3")
 call assert_almost_equal(l3ref, l3dist, __LINE__)
+
+ref = [-1.1717170062386328_dp, -1.1212220608364672E-003_dp, 1.6674369835388907E-003_dp, &
+    -1.6420893636515004_dp, -0.45661886467983320_dp, 0.68484198823320419_dp]
+rv = 1._dp
+rv(1:3) = rv(1:3) * 1000._dp
+ep = epoch(2000, 1, 1)
+lib = gcrftolib(rv, ep, earth, moon, "L2")
+call assert_almost_equal(lib, ref, __LINE__)
+eci = libtogcrf(lib, ep, earth, moon, "L2")
+call assert_almost_equal(eci, rv, __LINE__)
 
 end program testlibration
